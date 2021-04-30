8a11f2f5b1738818821aa68902fe81406b99a23c
claudion3 <claudion3@gmail.com>
2021-04-30 10:32:48 +0200

    update setting

--- contactAPI/settings.py
+++ contactAPI/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--0%etcg%1oqj8a5+fb$^4838=eqw8)+b43tv7^59-+525)ftx5'
+SECRET_KEY = config('SECRET_KEY')  
+ 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get('DEBUG', False)) 
-DEBUG=True
+DEBUG = config('DEBUG' ) 
+
 ALLOWED_HOSTS = []
 
 
@@ -138,8 +139,7 @@ USE_L10N = True
 USE_TZ = True
 
 #jwt
-JWT_SECRET_KEY= os.environ.get('JWT_SECRET_KEY')
-
+JWT_SECRET_KEY= config('JWT_SECRET_KEY')
 
 
 # Static files (CSS, JavaScript, Images)
